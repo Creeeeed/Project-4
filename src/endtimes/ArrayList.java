a8236ba9dc72d92baa57fd593b14b12ca2580b4b
Creeeeed <89224604+Creeeeed@users.noreply.github.com>
2022-10-26 09:00:07 -0600

    Update ArrayList.java

--- src/endtimes/ArrayList.java
+++ src/endtimes/ArrayList.java
@@ -1,107 +1,32 @@
 package endtimes;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+public class CustomArrayList <DesiredType> {
 
-public class ArrayList {
     Object[] array;
-    int elements = 0;
+    int numberOfElements; // stores a count of the number of elements in array
+
+    // all methods below must guarantee that
+    // numberOfElements < array.length.
+
+    public CustomArrayList( ) {
+        array = new Object[10];
+        numberOfElements = 0;
+    }
 
     public int size() {
-        return elements;
+        return numberOfElements;  // needs fixing
     }
 
     public boolean isEmpty() {
-        if (elements == 0){
-            return true;
-        }else{
-            return false;
-        }
-
-    }
-    public boolean contains(Object o) {
-        return false;
+        return numberOfElements == 0;
     }
 
-    public <T> T[] toArray(T[] a) {
-        return null;
+    public void clear( ){
+        numberOfElements = 0;
     }
 
-
-    public boolean add(Integer integer) {
-        array[elements++] = integer;
+    public void add(DesiredType item) {
+        array[numberOfElements++] = item;
     }
-
-    public boolean remove(Object o) {
-        return false;
-    }
-
-
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    public boolean addAll(int index, Collection<? extends Integer> c) {
-        return false;
-    }
-
-
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    public boolean retainAll(Collection<?> c) {
-        return false;
-    }
-
-
-    public void clear() {
-
-    }
-
-    public Integer get(int index) {
-        return null;
-    }
-
-
-    public Integer set(int index, Integer element) {
-        return null;
-    }
-
-
-    public void add(int index, Integer element) {
-
-    }
-
-    public Integer remove(int index) {
-        return null;
-    }
-
-    public int indexOf(Object o) {
-        return 0;
-    }
-
-
-    public int lastIndexOf(Object o) {
-        return 0;
-    }
-
-
-    public ListIterator<Integer> listIterator() {
-        return null;
-    }
-
-    public ListIterator<Integer> listIterator(int index) {
-        return null;
-    }
-
-    public List<Integer> subList(int fromIndex, int toIndex) {
-        return null;
-    }
-
-
-
 
 }
